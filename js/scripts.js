deed0a1659a34b6e05bc3b1e598d17ce1fcb9537
seanryano <seangerard.ryan@gonitro.com>
2021-06-11 11:58:37 +0100

    Google map

--- js/scripts.js
+++ js/scripts.js
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 53.0679786986982, lng: -7.217043543702605};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
