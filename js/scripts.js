b109131050e31aac41df11819716eaf70291cad3
seanryano <seangerard.ryan@gonitro.com>
2021-08-09 09:44:39 +0100

    Adding footer copyright

--- js/scripts.js
+++ js/scripts.js
@@ -486,7 +486,7 @@ $(document).ready(function(){
 });
 
 $(document).ready(function () {
-  $(".navbar-nav li a").click(function(event) {
+  $(".navbar-nav .menu-item a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
   });
 });
